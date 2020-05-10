2e15426aad06e5a7fdcde7bdfbb8906b264e335e
lcgaravito <lc.garavito@uniandes.edu.co>
2020-05-10 02:17:54 -0500

    bootstrap, google fonts and navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+
+// Components
+import Navigation from "./components/Navigation";
+import Game from "./components/Game";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <div className="container p-4">
+        <Route path="/" exact component={Home} />
+        <Route path="/game" component={Game} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
---
+++ front/src/components/Game.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Game() {
+  return (
+    <div>
+      <p>This is the Game!!!</p>
+    </div>
+  );
+}
