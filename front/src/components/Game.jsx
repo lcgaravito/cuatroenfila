2159d520d448ae80a28cf0e9f888299f04779648
lcgaravito <lc.garavito@uniandes.edu.co>
2020-05-10 20:28:42 -0500

    Game board animations

--- front/src/components/Game.jsx
+++ front/src/components/Game.jsx
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 import "./Game.css";
 
 const grid = [
-  [0, 0, 0, 0, 2, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 1, 0, 0],
-  [1, 1, 2, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
 ];
 
 export default function Game() {
   const [board, setBoard] = useState(grid);
+
+  const sendMotion = (i, j) => {
+    for (let index = board.length - 1; index >= 0; index--) {
+      const element = board[index][j];
+      if (element === 0) {
+        const boardCopy = [...board];
+        boardCopy[index][j] = 1;
+        setBoard(boardCopy);
+        return;
+      }
+    }
+  };
+
   return (
     <div className="row">
-      <div className="col-md-9">
+      <div className="col-md-8">
         <h2 className="text-center">Game Board</h2>
         <div className="game-board">
           <div className="row">
@@ -22,13 +35,22 @@ export default function Game() {
               row.map((value, j) =>
                 value === 0 ? (
                   <img
+                    className="image-token-empty"
                     key={`${i},${j}`}
                     src="token-empty250.png"
                     alt="Token empty"
+                    onMouseOver={(e) =>
+                      (e.currentTarget.src = "token-green250.png")
+                    }
+                    onMouseOut={(e) =>
+                      (e.currentTarget.src = "token-empty250.png")
+                    }
+                    onClick={(e) => sendMotion(i, j)}
                     width={`${100 / row.length}%`}
                   ></img>
                 ) : value === 1 ? (
                   <img
+                    className="image-token-green"
                     key={`${i},${j}`}
                     src="token-green250.png"
                     alt="Token green"
@@ -36,6 +58,7 @@ export default function Game() {
                   ></img>
                 ) : (
                   <img
+                    className="image-token-blue"
                     key={`${i},${j}`}
                     src="token-blue250.png"
                     alt="Token blue"
@@ -47,10 +70,10 @@ export default function Game() {
           </div>
         </div>
       </div>
-      <div className="col-md-3 game-info">
+      <div className="col-md-4 game-info">
         <h5 id="you">You</h5>
         <img src="token-green250.png" alt="Token green" width="80"></img>
-        <hr class="my-4"></hr>
+        <hr className="my-4"></hr>
         <h5 id="opponent">Opponent</h5>
         <img src="token-blue250.png" alt="Token blue" width="80"></img>
       </div>
